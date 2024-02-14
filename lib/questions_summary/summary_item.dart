d3bdb387f5c63553d7c2168da6c448969d68281e
dmytrosantarskyi <dsantarskyi@gmail.com>
2024-02-14 10:33:31 +0100

    completed project quiz app

---
+++ lib/questions_summary/summary_item.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/questions_summary/question_identifier.dart';
+
+class SummaryData extends StatelessWidget {
+  const SummaryData(this.itemData, {super.key});
+
+  final Map<String, Object> itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCorrectAnswer =
+        itemData['user_answer'] == itemData['correct_answer'];
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        QuestionIdentifier(
+          isCorrectAnswer: isCorrectAnswer,
+          questionIndex: itemData['question_index'] as int,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                itemData['question'] as String,
+                style: GoogleFonts.lato(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                itemData['user_answer'] as String,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 152, 139, 139),
+                ),
+              ),
+              Text(
+                itemData['correct_answer'] as String,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 144, 191, 228),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
